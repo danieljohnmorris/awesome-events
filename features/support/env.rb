61b3c8ce5355f673fbc71b760c361fac3ede5488
Daniel Morris <daniel.john.morris@googlemail.com>
2009-03-01 15:35:24 +0000

    Initial files from jeweler

---
+++ features/support/env.rb
@@ -0,0 +1,13 @@
+$LOAD_PATH.unshift(File.dirname(__FILE__) + '/../../lib')
+require 'awesome_events'
+
+require 'test/unit/assertions'
+
+require 'test/unit/assertions'
+
+World do |world|
+  
+  world.extend(Test::Unit::Assertions)
+  
+  world
+end
